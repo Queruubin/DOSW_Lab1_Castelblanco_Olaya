5ca6b39067fb19e13ff41b14ea4259bcaa382ddf
Samuel Castelblanco <samuelcf0123@gmail.com>
2026-02-03 22:25:10 -0500

    Lamba creada

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,5 +1,14 @@
 package Laboratorio1.reto2;
+import java.util.List;
+import java.util.function.BinaryOperator;
 
 public class CarreraParalela {
-    // Todo; hoola
+    public static void main(String[] args) {
+
+        BinaryOperator<Integer> mayor = (a, b) -> a > b ? a : b;
+
+        int max = List.of(3, 8, 6, 2).stream().reduce(mayor).orElse(null);
+
+        System.out.println("El número mayor es: " + max);
+    }
 }
