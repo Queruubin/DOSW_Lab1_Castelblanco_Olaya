d15139756763021ab915999fe89ea0e6fe0c2732
Tomas Olaya <tomasdi.dev@gmail.com>
2026-02-03 22:16:56 -0500

    feat(carril1): Estudiante B implementa calculo del numero maximo

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,5 +1,22 @@
-package Laboratorio1.reto2;
+package reto2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.IntSummaryStatistics;
 
 public class CarreraParalela {
-    // Todo; hoola
+
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(15, 2, 9, 21, 7, 3, 8, 4);
+        System.out.println("Listado de números: " + numeros);
+        
+        IntSummaryStatistics estadisticas = numeros.stream()
+            .mapToInt(num -> num)
+            .summaryStatistics();
+            
+        System.out.println("\n--- Resultados Carril 1 (Estudiante B) ---");
+        System.out.println("El número más grande es: " + estadisticas.getMax());
+        
+
+    }
 }
