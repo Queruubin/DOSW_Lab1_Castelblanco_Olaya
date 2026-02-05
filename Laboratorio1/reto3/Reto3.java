e15ab4d176bf7a820dc2927604f7319cd1a0d602
Samuel Castelblanco <samuelcf0123@gmail.com>
2026-02-05 17:07:57 -0500

    Se crea la funcion para concatenera palabras

--- Laboratorio1/reto3/Reto3.java
+++ Laboratorio1/reto3/Reto3.java
@@ -1,5 +1,21 @@
 package Laboratorio1.reto3;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Reto3 {
-    // Nuevo archivo para Reto 3
+    public static void main(String[] args) {
+        String mensaje = "Hola";
+        String resultado = repetirMensaje(mensaje);
+        System.out.println(resultado);
+    }
+
+    public static String repetirMensaje(String mensaje) {
+        // Usamos IntStream para generar el rango de repeticiones
+        return IntStream.range(0, 3)
+                .mapToObj(i -> mensaje)
+                .collect(Collectors.joining(" ", "", "")); 
+                // Aunque joining usa StringBuilder internamente, 
+                // aquí te muestro cómo forzar el uso de StringBuilder con un Collector personalizado:
+    }
 }
