54c5c49aca7b5b8165921f47f5e2fa26e8edbb1e
Tomas Olaya <tomasdi.dev@gmail.com>
2026-02-04 22:44:21 -0500

    feat(carril1): Estudiante B implementa calculo del numero minimo

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,4 +1,5 @@
-package Laboratorio1.reto2;
+package Laboratorio1.reto2; // Asegúrate que este package sea el correcto según tu estructura de carpetas
+
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
 import java.util.List;
@@ -7,22 +8,19 @@ import java.util.function.BinaryOperator;
 public class CarreraParalela {
     public static void main(String[] args) {
 
+        // Esta parte superior parece un ejemplo aparte, la dejo como estaba.
         BinaryOperator<Integer> mayor = (a, b) -> a > b ? a : b;
-
         int max = List.of(3, 8, 6, 2).stream().reduce(mayor).orElse(null);
+        System.out.println("El número mayor (ejemplo): " + max);
 
-        System.out.println("El número mayor es: " + max);
 
-        // Solución Estudiante B
-
+        // --- SOLUCIÓN ESTUDIANTE B (Modificada para buscar el MÍNIMO) ---
         List<Integer> numeros = Arrays.asList(15, 2, 9, 21, 7, 3, 8, 4);
-        System.out.println("Listado de números: " + numeros);
-        
+        System.out.println("\nListado de números: " + numeros);
         IntSummaryStatistics estadisticas = numeros.stream()
             .mapToInt(num -> num)
             .summaryStatistics();
-            
-        System.out.println("\n--- Resultados Carril 1 (Estudiante B) ---");
-        System.out.println("El número más grande es: " + estadisticas.getMax());
+        System.out.println("--- Resultados Carril 1 (Estudiante B) ---");
+        System.out.println("El número más pequeño es: " + estadisticas.getMin());
     }
 }
