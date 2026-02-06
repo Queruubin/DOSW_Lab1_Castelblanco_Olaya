f3b5abe3e5e5d00286dc556c3cd1a050efb51035
Samuel Castelblanco <samuelcf0123@gmail.com>
2026-02-05 22:01:04 -0500

    Se quitan comentarios innecesarios

--- Laboratorio 1/reto5/reto5.java
+++ Laboratorio 1/reto5/reto5.java
@@ -3,7 +3,6 @@ import java.util.stream.*;
 
 public class reto5 {
     public static void main(String[] args) {
-        // Datos de entrada del reto
         Set<Integer> hashSet = new HashSet<>(Arrays.asList(4, 9, 15, 7, 18, 21, 10, 5));
         Set<Integer> treeSet = new TreeSet<>(Arrays.asList(12, 3, 25, 10, 7, 30, 18, 4));
 
@@ -12,14 +11,12 @@ public class reto5 {
     }
 
     public static void reclamarArena(Set<Integer> hSet, Set<Integer> tSet) {
-        // 1. Unir ambas colecciones filtrando los múltiplos indicados
         Stream<Integer> streamA = hSet.stream().filter(n -> n % 3 != 0);
         Stream<Integer> streamB = tSet.stream().filter(n -> n % 5 != 0);
 
-        // 2. Concatenar, eliminar duplicados (con toSet) y ordenar
         Stream.concat(streamA, streamB)
-            .distinct() // Asegura que no haya guerreros repetidos
-            .sorted()   // Orden natural (ascendente)
+            .distinct()
+            .sorted()
             .forEach(n -> System.out.println("Número en arena: " + n));
     }
 }
