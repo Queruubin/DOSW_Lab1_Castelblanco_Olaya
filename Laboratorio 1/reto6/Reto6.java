5d3d80faa5cd073e0592f42fc64a86fd64d97c14
Samuel Castelblanco <samuelcf0123@gmail.com>
2026-02-06 18:56:46 -0500

    Se crea la funcion inicializarManual

--- Laboratorio 1/reto6/Reto6.java
+++ Laboratorio 1/reto6/Reto6.java
@@ -7,7 +7,6 @@ public class Reto6 {
     public static void main(String[] args) {
         inicializarManual();
         
-        // Pruebas de funcionamiento
         ejecutarComando("SALUDAR");
         ejecutarComando("BROMEAR");
     }
@@ -22,6 +21,9 @@ public class Reto6 {
     }
 
     public static void inicializarManual() {
-        // Aquí ocurrirá la magia (y el conflicto)
+        comandos.put("SALUDAR", () -> System.out.println("La máquina dice: ¡Saludos, viajero del tiempo y del código!"));
+        comandos.put("DESPEDIR", () -> System.out.println("La máquina dice: Que los bits te acompañen, hasta la próxima misión."));
+        comandos.put("CANTAR", () -> System.out.println("La máquina canta: 01010101"));
+        comandos.put("DANZAR", () -> System.out.println("La máquina gira y emite chispas: Girando en modo fiesta."));
     }
 }
