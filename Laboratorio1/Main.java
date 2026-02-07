cde0765e203402fc218ee40f3dd3f54db8e4a800
Samuel Castelblanco <samuelcf0123@gmail.com>
2026-02-07 10:53:18 -0500

    Se agrega archivo Main para ejcutar todos los retos

---
+++ Laboratorio1/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("=== RETO 1 ===");
+        reto1.Reto1.main(args);
+
+        System.out.println("=== RETO 2 ===");
+        reto2.CarreraParalela.main(args);
+
+        System.out.println("\n=== RETO 3 ===");
+        reto3.Reto3.main(args);
+
+        System.out.println("\n=== RETO 4 ===");
+        reto4.Reto4.main(args);
+
+        System.out.println("\n=== RETO 5 ===");
+        reto5.Reto5.main(args);
+
+        System.out.println("=== RETO 6 ===");
+        reto6.Reto6.main(args);
+    }
+}
